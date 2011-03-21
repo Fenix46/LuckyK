f8b4a6cc08d354cf8543e3ce09b4f5966cd1ae60
Thomas Gleixner <tglx@linutronix.de>
2011-03-21 12:33:18 +0100

    sched/rt: Use schedule_preempt_disabled()
    Coccinelle based conversion.
    
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Acked-by: Peter Zijlstra <a.p.zijlstra@chello.nl>
    Link: http://lkml.kernel.org/n/tip-24swm5zut3h9c4a6s46x8rws@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@elte.hu>

--- arch/x86/kernel/process_32.c
+++ arch/x86/kernel/process_32.c
@@ -115,9 +115,7 @@ void cpu_idle(void)
 		}
 		idle_notifier_call_chain(IDLE_END);
 		tick_nohz_restart_sched_tick();
-		preempt_enable_no_resched();
-		schedule();
-		preempt_disable();
+		schedule_preempt_disabled();
 	}
 }
 
