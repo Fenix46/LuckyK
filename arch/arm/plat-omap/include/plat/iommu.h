dc7930e23a810a5c7bd820ac4c0d06d0b14d9340
Ohad Ben-Cohen <ohad@wizery.com>
2011-08-17 22:29:46 +0300

    omap: iommu: remove unused exported API
    Remove unused public APIs from OMAP's iommu driver.
    
    IOMMU functionality should be exposed only via the generic IOMMU API;
    this way drivers stay generic, and different IOMMU drivers
    don't need to duplicate similar functionalities.
    
    The rest of the API still exposed by OMAP's iommu will be evaluated
    and eventually either added to the generic IOMMU API (if relevant),
    or completely removed.
    
    The intention is that OMAP's iommu driver will eventually not expose
    any public API.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Acked-by: Hiroshi DOYU <Hiroshi.DOYU@nokia.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Signed-off-by: Joerg Roedel <joerg.roedel@amd.com>

--- arch/arm/plat-omap/include/plat/iommu.h
+++ arch/arm/plat-omap/include/plat/iommu.h
@@ -162,12 +162,9 @@ struct iommu_platform_data {
 extern u32 iommu_arch_version(void);
 
 extern void iotlb_cr_to_e(struct cr_regs *cr, struct iotlb_entry *e);
-extern void iommu_set_twl(struct iommu *obj, bool on);
-extern void flush_iotlb_range(struct iommu *obj, u32 start, u32 end);
 
 extern int iopgtable_store_entry(struct iommu *obj, struct iotlb_entry *e);
 
-extern int iommu_set_da_range(struct iommu *obj, u32 start, u32 end);
 extern struct iommu *iommu_get(const char *name);
 extern void iommu_put(struct iommu *obj);
 extern int iommu_set_isr(const char *name,
--- drivers/iommu/omap-iommu.c
+++ drivers/iommu/omap-iommu.c
@@ -344,26 +344,6 @@ static void flush_iotlb_page(struct iommu *obj, u32 da)
 }
 
 /**
- * flush_iotlb_range - Clear an iommu tlb entries
- * @obj:	target iommu
- * @start:	iommu device virtual address(start)
- * @end:	iommu device virtual address(end)
- *
- * Clear an iommu tlb entry which includes 'da' address.
- **/
-void flush_iotlb_range(struct iommu *obj, u32 start, u32 end)
-{
-	u32 da = start;
-
-	while (da < end) {
-		flush_iotlb_page(obj, da);
-		/* FIXME: Optimize for multiple page size */
-		da += IOPTE_SIZE;
-	}
-}
-EXPORT_SYMBOL_GPL(flush_iotlb_range);
-
-/**
  * flush_iotlb_all - Clear all iommu tlb entries
  * @obj:	target iommu
  **/
@@ -377,21 +357,6 @@ static void flush_iotlb_all(struct iommu *obj)
 
 	iommu_write_reg(obj, 1, MMU_GFLUSH);
 }
-
-/**
- * iommu_set_twl - enable/disable table walking logic
- * @obj:	target iommu
- * @on:		enable/disable
- *
- * Function used to enable/disable TWL. If one wants to work
- * exclusively with locked TLB entries and receive notifications
- * for TLB miss then call this function to disable TWL.
- */
-void iommu_set_twl(struct iommu *obj, bool on)
-{
-	arch_iommu->set_twl(obj, on);
-}
-EXPORT_SYMBOL_GPL(iommu_set_twl);
 
 #if defined(CONFIG_OMAP_IOMMU_DEBUG_MODULE)
 
@@ -827,28 +792,6 @@ static int device_match_by_alias(struct device *dev, void *data)
 
 	return strcmp(obj->name, name) == 0;
 }
-
-/**
- * iommu_set_da_range - Set a valid device address range
- * @obj:		target iommu
- * @start		Start of valid range
- * @end			End of valid range
- **/
-int iommu_set_da_range(struct iommu *obj, u32 start, u32 end)
-{
-
-	if (!obj)
-		return -EIO;
-
-	if (end < start || !PAGE_ALIGN(start | end))
-		return -EINVAL;
-
-	obj->da_start = start;
-	obj->da_end = end;
-
-	return 0;
-}
-EXPORT_SYMBOL_GPL(iommu_set_da_range);
 
 /**
  * _set_latency_cstr - set a latency constraint in the proper pwrdm
