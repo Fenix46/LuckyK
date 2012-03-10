b432268a2a3b10f08b7e65c75aa3a3ac058d161c
Colin Cross <ccross@android.com>
2012-03-09 20:07:04 -0800

    android: persistent_ram: fix section mismatch in clients
    persistent_ram clients will call the init functions from their
    probe functions, which need to be __devinit, not __init.  Change
    the persistent_ram init functions to be __devinit.
    
    Change-Id: If92338f7c8d57dd34b286ff7705a96c078477ba4
    Signed-off-by: Colin Cross <ccross@android.com>

--- drivers/staging/android/persistent_ram.c
+++ drivers/staging/android/persistent_ram.c
@@ -34,7 +34,7 @@ struct persistent_ram_buffer {
 
 #define PERSISTENT_RAM_SIG (0x43474244) /* DBGC */
 
-static __initdata LIST_HEAD(persistent_ram_list);
+static __devinitdata LIST_HEAD(persistent_ram_list);
 
 static void persistent_ram_encode_rs8(struct persistent_ram_zone *prz,
 	uint8_t *data, size_t len, uint8_t *ecc)
@@ -206,7 +206,7 @@ static void persistent_ram_update(struct persistent_ram_zone *prz,
 	persistent_ram_update_ecc(prz, count);
 }
 
-static void __init
+static void __devinit
 persistent_ram_save_old(struct persistent_ram_zone *prz)
 {
 	struct persistent_ram_buffer *buffer = prz->buffer;
@@ -313,7 +313,7 @@ static int persistent_ram_buffer_map(phys_addr_t start, phys_addr_t size,
 	return 0;
 }
 
-static int __init persistent_ram_buffer_init(const char *name,
+static int __devinit persistent_ram_buffer_init(const char *name,
 		struct persistent_ram_zone *prz)
 {
 	int i;
@@ -335,7 +335,7 @@ static int __init persistent_ram_buffer_init(const char *name,
 	return -EINVAL;
 }
 
-static  __init
+static  __devinit
 struct persistent_ram_zone *__persistent_ram_init(struct device *dev, bool ecc)
 {
 	struct persistent_ram_zone *prz;
@@ -382,7 +382,7 @@ struct persistent_ram_zone *__persistent_ram_init(struct device *dev, bool ecc)
 	return prz;
 }
 
-struct persistent_ram_zone * __init
+struct persistent_ram_zone * __devinit
 persistent_ram_init_ringbuffer(struct device *dev, bool ecc)
 {
 	return __persistent_ram_init(dev, ecc);
